e1972d567a8c1cca1111fa29cc0b065cb261651c
foxjazz <foxr4@foxjazz.net>
2017-01-22 17:38:52 -0700

    made the nav links actual buttons to check if some login logic worked.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import {NavbarComponent} from "./navbar/navbar.component";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   providers: [NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './members/members.component.css']
 })
 export class AppComponent {
 
-  title = 'app works!';
-
-  ngOnInit(){
-    this.title = 'Secular Hub Members';
+  constructor(public router: Router) { }
+  title: string;
+  login: false;
+  jwt: string;
+  routeToMembers(){
+    this.jwt = localStorage.getItem('id_token');
+    if(this.jwt.length > 0)
+    {
+      this.router.navigate(['/members']);
+    }
   }
+  routeToMaintenance(){
+    this.jwt = localStorage.getItem('id_token');
+    if(this.jwt.length > 0)
+    {
+      this.router.navigate(['/maintenance']);
+    }
+  }
+  ngOnInit(){
+    this.title ='Secular Hub Members';
+    localStorage.setItem('id_token', '');
+  }
+
 }
